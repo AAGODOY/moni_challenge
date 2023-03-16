eb6e52a87dd7240da1f8fd21fb578caac49d9f73
AAGODOY <48739855+AAGODOY@users.noreply.github.com>
2023-03-16 18:45:18 -0300

    add order selectors on home page

--- src/pages/HomePage.py
+++ src/pages/HomePage.py
@@ -6,6 +6,8 @@ class HomePage(BasePage):
     CLOSE_MODAL_NOTIFICATION_BUTTON = 'btn-close'
     HEADER_PROFILE_TEXT = '#header-profile > a > span'
     LOGOUT_BUTTON = 'header-logout'
+    MENU_SALE_BUTTON = 'menu-sale'
+    ORDER_OPTION_BUTTON = '//li[@id="menu-sale"]/ul/li/a[text()="Orders"]'
 
     def click_on_close_modal_notification_button(self):
         element = WebDriver().get_driver().find_element(By.CLASS_NAME, 
@@ -18,4 +20,12 @@ class HomePage(BasePage):
 
     def click_on_logout_button(self):
         element = WebDriver().get_driver().find_element(By.ID, self.LOGOUT_BUTTON)
+        BasePage().click_element(element)
+
+    def click_on_menu_sale_button(self):
+        element = WebDriver().get_driver().find_element(By.ID, self.MENU_SALE_BUTTON)
+        BasePage().click_element(element)
+    
+    def click_on_order_option_button(self):
+        element = WebDriver().get_driver().find_element(By.XPATH, self.ORDER_OPTION_BUTTON)
         BasePage().click_element(element)
